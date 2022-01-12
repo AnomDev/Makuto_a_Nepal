17dca42322f7d242ab438ea881dffc08242dcada
awoisoak <awoisoak@gmail.com>
2022-01-12 20:19:59 +0100

    Fix adapter using viewbinding

--- app/src/main/java/com/anomdev/makutoanepal/ui/fragments/home/PostsFeedRVAdapter.kt
+++ app/src/main/java/com/anomdev/makutoanepal/ui/fragments/home/PostsFeedRVAdapter.kt
@@ -1,23 +1,19 @@
 package com.anomdev.makutoanepal.ui.fragments.home
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.anomdev.makutoanepal.R
-import com.anomdev.makutoanepal.databinding.ItemBlogPostBinding
+import com.anomdev.makutoanepal.databinding.FragmentHomeBinding
 import com.anomdev.makutoanepal.model.BlogPost
-import com.squareup.picasso.Picasso
 
 
 class PostsFeedRVAdapter (val feedList:List<BlogPost>): RecyclerView.Adapter<PostsFeedRVAdapter.PostHolder>(){
 
-   private val title = R.id.tvTitle
 
     //Éste método crea el ViewHolder, en este caso el PostHolder
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        return PostHolder(layoutInflater.inflate(R.layout.item_blog_post, parent, false))
+        val binding = FragmentHomeBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return PostHolder(binding)
     }
 
     //Éste método dice cuántos items tiene el RV
@@ -32,12 +28,11 @@ class PostsFeedRVAdapter (val feedList:List<BlogPost>): RecyclerView.Adapter<Pos
 
 
 
-    class PostHolder(private val view : View): RecyclerView.ViewHolder(view){
+    class PostHolder(private val binding : FragmentHomeBinding): RecyclerView.ViewHolder(binding.root){
 
         fun render(feedList: BlogPost) {
-            //TODO Mejorar usando sólo "ItemBlogPostBinding"
-            (view as ItemBlogPostBinding).tvTitle.text = feedList.title
-            Picasso.get().load(feedList.image).into( (view as ItemBlogPostBinding).ivPost)
+            binding.titleTv.text = feedList.title
+//            Picasso.get().load(feedList.image).into( (view as ItemBlogPostBinding).ivPost)
         }
     }
 
