c1e24499fd55611ee7138d2543a5aa9839f8367b
AnomDev <serbla.dev@gmail.com>
2022-02-07 02:14:06 +0100

    MapFragment added
    w/ markers, polylines, and zoom animation

--- app/src/main/java/com/anomdev/makutoanepal/ui/fragments/map/MapFragment.kt
+++ app/src/main/java/com/anomdev/makutoanepal/ui/fragments/map/MapFragment.kt
@@ -1,63 +1,314 @@
 package com.anomdev.makutoanepal.ui.fragments.map
 
-import android.content.Intent
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.anomdev.makutoanepal.R
-import com.anomdev.makutoanepal.ui.MainActivity
+import com.anomdev.makutoanepal.databinding.FragmentMapBinding
+import com.cocoahero.android.geojson.GeoJSON
+import com.cocoahero.android.geojson.GeoJSONObject
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.*
+import org.json.JSONObject
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [MapFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class MapFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class MapFragment : Fragment(), OnMapReadyCallback {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private var _binding: FragmentMapBinding? = null
+    private val binding get() = _binding!!
 
-        }
-    }
+    private lateinit var map: GoogleMap
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        _binding = FragmentMapBinding.inflate(inflater, container, false)
+
+
+        //TODO:
+        val mapFragment =
+            childFragmentManager.findFragmentById(R.id.fragmentMap) as SupportMapFragment?  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment!!.getMapAsync { mMap ->
+            mMap.mapType = GoogleMap.MAP_TYPE_NORMAL
+            mMap.clear() //clear old markers
+
+
+            onMapReady(mMap)
+
+            val nepalZoom = CameraPosition.builder()
+                .target(
+                    LatLng(
+                        getString(R.string.INIT_LAT).toDouble(),
+                        getString(R.string.INIT_LNG).toDouble()
+                    )
+                )
+                .zoom(6f)
+                .bearing(0f)
+                .tilt(45f)
+                .build()
+
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(nepalZoom), 5000, null)
+
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.SWAYAMBHU_LAT).toDouble(),
+                            getString(R.string.SWAYAMBHU_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.swayambhu))
+                    .snippet(getString(R.string.swayambhu_snippet))
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.BOUDHA_LAT).toDouble(),
+                            getString(R.string.BOUDHA_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.boudha))
+                    .snippet(getString(R.string.boudha_snippet))
+
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.PASHU_LAT).toDouble(),
+                            getString(R.string.PASHU_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.pashu))
+                    .snippet(getString(R.string.pashu_snippet))
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.CHITWAN_LAT).toDouble(),
+                            getString(R.string.CHITWAN_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.chitwan))
+                    .snippet(getString(R.string.chitwan_snippet))
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.LUMBINI_LAT).toDouble(),
+                            getString(R.string.LUMBINI_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.lumbini))
+                    .snippet(getString(R.string.lumbini_snippet))
+            )
+
+            //TREKKINGS
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.EBC_LAT).toDouble(),
+                            getString(R.string.EBC_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.ebc))
+                    .snippet(getString(R.string.ebc_snippet))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.DHAULA_LAT).toDouble(),
+                            getString(R.string.DHAULA_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.dhaula))
+                    .snippet(getString(R.string.dhaula_snippet))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+            )
+
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.POON_LAT).toDouble(),
+                            getString(R.string.POON_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.poon))
+                    .snippet(getString(R.string.poon_snippet))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ORANGE))
+            )
+
+            //NGOs
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.NEPALSONRIE_LAT).toDouble(),
+                            getString(R.string.NEPALSONRIE_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.nepalsonrie))
+                    .snippet(getString(R.string.nepalsonrie_snippet))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+            )
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.UDANA_LAT).toDouble(),
+                            getString(R.string.UDANA_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.udana))
+                    .snippet(getString(R.string.udana_snippet))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+            )
+            //BUSINESS
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.NISCHAL_LAT).toDouble(),
+                            getString(R.string.NISCHAL_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.nischal_guesthouse))
+                    .snippet(getString(R.string.nischal_guesthouse_snippet))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            )
+            mMap.addMarker(
+                MarkerOptions()
+                    .position(
+                        LatLng(
+                            getString(R.string.NOTTOPVIEW_LAT).toDouble(),
+                            getString(R.string.NOTTOPVIEW_LNG).toDouble()
+                        )
+                    )
+                    .title(getString(R.string.nottopview_restaurant))
+                    .snippet(getString(R.string.nottopview_restaurant_snippet))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+            )
+        }
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment MapFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            MapFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun createPolylines() {
+        val polylineOptions = PolylineOptions()
+            .add(
+                LatLng(
+                    28.8823635,
+                    80.0612584
+                )
+            )
+            .add(
+                LatLng(
+                    27.6550433,
+                    82.7212214
+                )
+            )
+            .add(
+                LatLng(
+                    26.9971063,
+                    84.8568961,
+                )
+            )
+            .add(
+                LatLng(
+                    26.368344,
+                    87.2977277
+
+                )
+            )
+            .add(
+                LatLng(
+                    27.2485367,
+                    88.0273333
+                )
+            )
+
+            .add(
+                LatLng(
+                    27.9125569,
+                    85.999186
+                )
+            )
+            .add(
+                LatLng(
+
+                    30.0485139,
+                    82.3227869
+                )
+            )
+            .add(
+                LatLng(
+                    30.0651529,
+                    81.0986009
+
+                )
+            )
+            .add(
+                LatLng(
+                    28.8823635,
+                    80.0612584
+
+                )
+            )
+            .width(4f)
+            .color(ContextCompat.getColor(requireContext(), R.color.secondary_dark))
+
+        val polyline = map.addPolyline(polylineOptions)
+        polyline.startCap = RoundCap()
+        polyline.endCap = RoundCap()
     }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        createPolylines()
+    }
+
+//    private fun bitmapDescriptorFromVector(context: Context?, vectorResId: Int): BitmapDescriptor {
+//        val vectorDrawable = ContextCompat.getDrawable(context!!, vectorResId)
+//        vectorDrawable!!.setBounds(
+//            0,
+//            0,
+//            vectorDrawable.intrinsicWidth,
+//            vectorDrawable.intrinsicHeight
+//        )
+//        val bitmap =
+//            Bitmap.createBitmap(
+//                vectorDrawable.intrinsicWidth,
+//                vectorDrawable.intrinsicHeight,
+//                Bitmap.Config.ARGB_8888
+//            )
+//        val canvas = Canvas(bitmap)
+//        vectorDrawable.draw(canvas)
+//        return BitmapDescriptorFactory.fromBitmap(bitmap)
+//    }
+
+
 }
